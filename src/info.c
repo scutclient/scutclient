6aacdd26819fa28a318125d37a0171f699a4e443
Chuanhong Guo <gch981213@gmail.com>
2018-03-26 23:11:38 +0800

    删除两个无用函数

--- src/info.c
+++ src/info.c
@@ -12,26 +12,6 @@ char *Hash = "2ec15ad258aee9604b18f2f8114da38db16efd00";
 unsigned char Version[64] = { 0x44, 0x72, 0x43, 0x4f, 0x4d, 0x00, 0x96, 0x02, 0x2a };
 int Version_len = 9;
 
-int trim(char s[]) {
-	int n;
-	for (n = strlen(s) - 1; n >= 0; n--) {
-		if (s[n] != ' ' && s[n] != '\t' && s[n] != '\n' && s[n] != '\r') {
-			break;
-		}
-		s[n] = '\0';
-	}
-	return n;
-}
-
-int checkInit(uint8_t info[], int infoLen) {
-	int i = 0;
-	int result = 0;
-	for (i = 0; i < infoLen; i++) {
-		result += info[i];
-	}
-	return result;
-}
-
 void hexStrToByte(const char* source, unsigned char* dest, int sourceLen) {
 	short i;
 	unsigned char highByte, lowByte;
